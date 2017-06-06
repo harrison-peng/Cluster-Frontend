f3f7ea997cb561905a72f1834ac8f679f61ae862
Allen Chang <Allen@AllendeMacBook-Pro-2.local>
2017-06-06 15:12:01 +0800

    fix js

--- app/club/chatroom.js
+++ app/club/chatroom.js
@@ -1,21 +1,14 @@
 var ChatEngine = function() {
-    var name = " ";
+    var name = "";
     var msg = "";
     var chatZone = document.getElementById("chatZone");
     var oldata = "";
     var sevr = " ";
     var xhr = " ";
     //initialzation
-    this.init = function() {
-        if (EventSource) {
+    this.init = function() { 
             this.setName();
-            this.initSevr();
-        } else {
-            alert("Use latest Chrome or FireFox");
-        }
     };
-    //Setting user name
-    
     //For sending message
     this.sendMsg = function() {
         msg = document.getElementById("msg").value;
@@ -25,7 +18,22 @@ var ChatEngine = function() {
         return false;
     };
     //sending message to server
-    
+    this.ajaxSent = function() {
+        try {
+            xhr = new XMLHttpRequest();
+        } catch (err) {
+            alert(err);
+        }
+        xhr.open('GET', 'chatprocess.php?msg=' + msg + '&name=' + name, false);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    msg.value = "";
+                }
+            }
+        };
+        xhr.send();
+    };
     //HTML5 SSE(Server Sent Event) initilization
     this.initSevr = function() {
         sevr = new EventSource('chatprocess.php');

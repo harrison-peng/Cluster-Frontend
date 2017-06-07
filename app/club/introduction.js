d6e2a717acfa10fafd8f2b46a96972628f01b58d
travis Huang <travis965@gmail.com>
2017-06-07 19:30:42 +0800

    change-7

--- app/club/introduction.js
+++ app/club/introduction.js
@@ -5,8 +5,7 @@ $(document).ready(function() {
         dataType: "json",
         success: function(msg) {
             console.log(msg);
-            var item = "<div class='col-xs-8 col-sm-8 col-md-8 col-lg-8'><h3>" + msg[0].name + "</h3><h5>" + msg[0].place + "</h5><p>" + msg[0].address + "</p><br><p>5 人將參加</p></div><h3>" + msg[0].meeting_time + "</h3>";
-
+            var item = "<div class='col-xs-8 col-sm-8 col-md-8 col-lg-8'><h3>" + msg[0].name + "</h3><h5>" + msg[0].place + "</h5><p>" + msg[0].address + "</p><br><p>5 人將參加</p></div><h3>"+msg[0].meeting_time.slice(0,msg[0].meeting_time.indexOf(","))+"</h3><h3>"+msg[0].meeting_time.slice(msg[0].meeting_time.indexOf(",")+1)+"</h3></div>";
             $('.recentMeet').append(item);
         }
     });

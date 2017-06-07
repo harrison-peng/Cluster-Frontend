23d63a695ec2eb6ce71b41a8fd3fee2055459b50
travis Huang <travis965@gmail.com>
2017-06-07 18:48:48 +0800

    changeMemberArrange

--- app/club/member.js
+++ app/club/member.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
             console.log(msg);
 
             msg.forEach(function(element) {
-               var item = "<div class='col-xs-3 col-sm-3 col-md-3 col-lg-3'><div class='panel panel-default'><div class='panel-heading'><img src='" + element.photo + "' class='img-responsive' alt='Image'></div><div class='panel-body text-center'>" + element.name + "</div></div></div>"; 
+               var item = "<div class='col-xs-3 col-sm-3 col-md-3 col-lg-3'><div class='panel panel-default'><div class='panel-heading'><img src='" + element.photo + "' class='img-responsive' alt='Image'></div><div class='panel-body text-center'>" + element.name + "</div></div></div>";
 
                $('.memberList').append(item);
             }, this);

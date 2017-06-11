808649a3738cf5c904edfaf0d43725d50d8c81fd
travis Huang <travis965@gmail.com>
2017-06-12 01:55:39 +0800

    finishlogin

--- app/signin.js
+++ app/signin.js
@@ -2,73 +2,86 @@
 //     alert("google");
 // });
 
+// var fr = new FileReader();
+// fr.onload = function(e) {
+//     //e.target.result = base64 format picture
+//     console.log(e.target.result);
+//     // console.dir(e);
+//     // $('#img').attr( "src", e.target.result );
+// };
+// fr.readAsDataURL( this.files[0] );
+// // console.dir(document.getElementById('uploadFile'))
+// // console.dir(event);
+
 document.getElementsByClassName('btn-facebook')[0].addEventListener("click", function(){
     var provider = new firebase.auth.FacebookAuthProvider();
     firebase.auth().signInWithPopup(provider).then(function(result) {
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
         alert("Hallo!!"+ result.user.displayName+"!!Welcome!!");
+        var token = result.credential.accessToken;
+        // console.log(token);
+
+
+        let data = {
+            'fbtoken': token
+        }
 
         let options = {
-            method: "GET",
+            method: "POST",
+            body: JSON.stringify(data),
             // body: JSON.stringify(data),
             headers: {
-            "Content-Type": "application/json"
+                "Content-Type": "application/json"
             },
             // credentials: "same-origin"
         }
-        var token = result.credential.accessToken;
-        console.log(token);
+        fetch("http://localhost:3000/members", options).then(function(response) {
+            response.json().then(function(result){
+                // console.log(result);
+                sessionStorage.setItem("user", JSON.stringify(result));
 
-        fetch("https://graph.facebook.com/v2.9/me/picture?redirect=false&access_token="+token, options).then(function(response) {
-            response.json().then(function(picture){
-                console.log(picture.data.url);
-                function toDataURL(url, callback) {
-                  var xhr = new XMLHttpRequest();
-                  xhr.onload = function() {
-                    var reader = new FileReader();
-                    reader.onloadend = function() {
-                      callback(reader.result);
-                    }
-                    reader.readAsDataURL(xhr.response);
-                  };
-                  xhr.open('GET', url);
-                  xhr.responseType = 'blob';
-                  xhr.send();
+                if(sessionStorage.getItem("user")){
+                    // console.log("GET!!!"+sessionStorage.getItem("user"));
+                    firebase.auth().signOut().then(ele=>{
+
+                        alert("Success login,firebase logout");
+                        window.history.go(-1);
+                    }).catch(err=>{
+                        alert(err);
+                        window.history.go(-1);
+                    });
+
+                }else{
+                    alert("woops");
+                    window.history.go(-1);
                 }
 
-                toDataURL(picture.data.url, function(dataUrl) {
-                  console.log('RESULT:', dataUrl)
-                })
-
-
-            },function(error) {
-            // handle network error
-                console.log(error);
+            }).catch(function(err){
+                alert(err);
+                window.history.go(-1);
             });
-
-            var user = result.user.displayName;
-            console.log(user);
-            sessionStorage.setItem("user", user);
-            console.log("SET!!!"+sessionStorage.getItem("user"));
-        // handle HTTP response
-        }, function(error) {
-        // handle network error
-            console.log(error);
+        }).catch(function(err){
+            alert("woops!!ServerError");
+            window.history.go(-1);
         });
+
+
 
         // The signed-in user info.
 
         //window.history.go(-1);
         // ...
     }).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // The email of the user's account used.
-      var email = error.email;
-      // The firebase.auth.AuthCredential type that was used.
-      var credential = error.credential;
-      // ...
+        alert(error);
+        // // Handle Errors here.
+        // var errorCode = error.code;
+        // var errorMessage = error.message;
+        // // The email of the user's account used.
+        // var email = error.email;
+        // // The firebase.auth.AuthCredential type that was used.
+        // var credential = error.credential;
+        // // ...
+        window.history.go(-1);
     });
 });
 

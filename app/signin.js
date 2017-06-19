3ab9f70c401eaa65363122cced07acef3dcd7091
travis Huang <travis965@gmail.com>
2017-06-19 18:37:41 +0800

    addGoogleSignIn

--- app/signin.js
+++ app/signin.js
@@ -1,6 +1,76 @@
-// document.getElementsByClassName('btn-google')[0].addEventListener("click", function(){
-//     alert("google");
-// });
+window.addEventListener('pageshow', function(event) {
+    if(sessionStorage.getItem('user')){
+        $.get("/Cluster-Frontend/view/navBarLogin.html",function(data){
+            $("#navbar").html(data);
+        });
+    }else{
+        $.get("/Cluster-Frontend/view/navBar.html",function(data){
+            $("#navbar").html(data);
+            document.getElementById('signin').style.display = 'none'
+        });
+    }
+});
+
+var add_update_member = function(options) {
+    fetch("http://localhost:3000/members", options).then(function(response) {
+        response.json().then(function(result){
+            // console.log(result);
+            sessionStorage.setItem("user", JSON.stringify(result));
+
+            if(sessionStorage.getItem("user")){
+                // console.log("GET!!!"+sessionStorage.getItem("user"));
+                firebase.auth().signOut().then(ele=>{
+
+                    // alert("Success login,firebase logout");
+                    alert("Success Login!!");
+                    window.history.go(-1);
+                }).catch(err=>{
+                    alert(err);
+                    window.history.go(-1);
+                });
+
+            }else{
+                alert("woops");
+                window.history.go(-1);
+            }
+
+        }).catch(function(err){
+            alert(err);
+            window.history.go(-1);
+        });
+    }).catch(function(err){
+        alert("woops!!ServerError");
+        window.history.go(-1);
+    });
+}
+
+
+document.getElementsByClassName('btn-google')[0].addEventListener("click", function(){
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        alert("Hallo!!"+ result.user.displayName+"!!Welcome!!");
+
+        var token = result.credential.accessToken;
+        console.log(token);
+
+        let data = {
+            'googletoken': token
+        }
+
+        let options = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json"
+            },
+        }
+        add_update_member(options);
+
+    }).catch(function(error) {
+        console.log(error);
+    });
+});
 
 // var fr = new FileReader();
 // fr.onload = function(e) {
@@ -21,7 +91,6 @@ document.getElementsByClassName('btn-facebook')[0].addEventListener("click", fun
         var token = result.credential.accessToken;
         // console.log(token);
 
-
         let data = {
             'fbtoken': token
         }
@@ -35,39 +104,11 @@ document.getElementsByClassName('btn-facebook')[0].addEventListener("click", fun
             },
             // credentials: "same-origin"
         }
-        fetch("https://gotoclusterapi.herokuapp.com/members", options).then(function(response) {
-            response.json().then(function(result){
-                // console.log(result);
-                sessionStorage.setItem("user", JSON.stringify(result));
-
-                if(sessionStorage.getItem("user")){
-                    // console.log("GET!!!"+sessionStorage.getItem("user"));
-                    firebase.auth().signOut().then(ele=>{
-
-                        // alert("Success login,firebase logout");
-                        alert("Success Login!!");
-                        window.history.go(-1);
-                    }).catch(err=>{
-                        alert(err);
-                        window.history.go(-1);
-                    });
-
-                }else{
-                    alert("woops");
-                    window.history.go(-1);
-                }
-
-            }).catch(function(err){
-                alert(err);
-                window.history.go(-1);
-            });
-        }).catch(function(err){
-            alert("woops!!ServerError");
-            window.history.go(-1);
-        });
+        add_update_member(options);
 
     }).catch(function(error) {
-        alert(error);
+        console.log(error);
+
         // // Handle Errors here.
         // var errorCode = error.code;
         // var errorMessage = error.message;
@@ -76,8 +117,5 @@ document.getElementsByClassName('btn-facebook')[0].addEventListener("click", fun
         // // The firebase.auth.AuthCredential type that was used.
         // var credential = error.credential;
         // // ...
-        window.history.go(-1);
     });
 });
-
-setTimeout(function(){ document.getElementById('signin').style.display = 'none'; }, 10);
